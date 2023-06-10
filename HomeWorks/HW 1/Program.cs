1a9e2abc3c8d08ddbfe9d192db64a13a77723d06
AnastasiaK <Fair996@mail.ru>
2023-06-10 21:11:15 +0500

    4 задания домашней работы

---
+++ HomeWorks/HW 1/Program.cs
@@ -0,0 +1,72 @@
+﻿//Task 2
+
+// System.Console.Write("Введите первое число -> ");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// System.Console.Write("Введите второе число -> ");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+
+// if (num1 == num2)
+// {
+//    System.Console.Write($"Введенные числа равны");
+// }
+// else if(num1 > num2)
+//     {
+//         System.Console.Write($"Первое число -> {num1} больше, а второе число -> {num2}  меньше ");
+//     }
+
+//     else
+//     {
+//         System.Console.Write($"Второе число -> {num2} больше, а первое число -> {num1} меньше");
+//     }
+
+// Task 4
+
+// System.Console.Write("Введите первое число -> ");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// System.Console.Write("Введите второе число -> ");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+// System.Console.Write("Введите третье число -> ");
+// int num3 = Convert.ToInt32(Console.ReadLine());
+// int max = num1;
+
+// if(max < num2)
+//     {
+//         max = num2;
+//     }
+//         if (max < num3)
+//         {
+//             max = num3;
+//         }
+        
+// System.Console.Write($"Максимальное число {max}");
+
+//Task 6
+
+// System.Console.Write("Введите число -> ");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// if (num % 2 == 0)
+//     {
+//         System.Console.Write($"Число {num} четное");
+//     }
+// else
+//     {
+//         System.Console.Write($"Число {num} нечетное");
+//     }
+
+//Task 8
+
+// System.Console.Write("Введите число -> ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// int current = 1;
+
+
+// while (current <= n )
+// {
+//     if (current % 2 == 0)
+//     {
+//        System.Console.Write(current);
+//     }
+//     current ++;
+
+// }
